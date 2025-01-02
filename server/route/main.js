be7a59f67fe0aaca98280b65ccaf0dd13e6a9630
Eduardo Destruti <limaedu@gmail.com>
2025-01-02 14:40:35 -0500

    project upgrade

--- server/route/main.js
+++ server/route/main.js
@@ -8,7 +8,7 @@ router.get('', async (req, res) => {
 
         const locals = {
             title: "Destruti Website",
-            description: "This is a new Destruti Website build in NodeJs and EJS",
+            description: "This is a personal Destruti Website <a href='https://github.com/destruti/website' target='_blank'>(Github)</a>" ,
         }
         // const data = await Post.find();
 
