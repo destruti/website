c03e9fa63f52e12db096926f2bc63f6f0ed477d5
Eduardo Destruti <limaedu@gmail.com>
2025-01-02 16:32:43 -0500

    project upgrade

--- server/route/main.js
+++ server/route/main.js
@@ -90,9 +90,63 @@ router.post('/search', async (req, res) => {
 });
 
 
-router.get('/about', (req, res) => {
+router.get('/about', async (req, res) => {
 
-    res.render('about');
+    const locals = {
+        title: "Destruti Website",
+        description: "This is a personal Destruti Website <a href='https://github.com/destruti/website' target='_blank'>(Github)</a>" ,
+    }
+    res.render('pages/about', {
+        locals,
+    });
+
+});
+
+router.get('/courses', async (req, res) => {
+
+    const locals = {
+        title: "Destruti Website",
+        description: "This is a personal Destruti Website <a href='https://github.com/destruti/website' target='_blank'>(Github)</a>" ,
+    }
+    res.render('pages/courses', {
+        locals,
+    });
+
+});
+
+router.get('/posts', async (req, res) => {
+
+    try {
+
+        const locals = {
+            title: "Destruti Website",
+            description: "This is a personal Destruti Website <a href='https://github.com/destruti/website' target='_blank'>(Github)</a>" ,
+        }
+        // const data = await Post.find();
+
+        let perPage = 10;
+        let page = req.query.page || 1;
+
+        const data = await Post.aggregate([{ $sort: { createdAt: -1 } }])
+            .skip(perPage * page - perPage)
+            .limit(perPage)
+            .exec();
+
+        const count = await Post.countDocuments({});
+        const nextPage = parseInt(page) + 1;
+        const hasNextPage = nextPage <= Math.ceil(count / perPage);
+
+        res.render('pages/posts', {
+            locals,
+            data,
+            current: page,
+            nextPage: hasNextPage ? nextPage : null
+        });
+
+    } catch (error) {
+        console.log(error)
+    }
+
 
 });
 
